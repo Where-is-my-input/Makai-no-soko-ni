0c541f778e320837d9c86b6847705fec1ede3a08
Henrique Alberto de Andrade <henrique.alberto.andrade@hotmail.com>
2024-08-06 21:17:49 -0300

    #7

---
+++ Scenes/collision_objects/moving_platform.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@onready var animation_player = $Path2D/AnimationPlayer
+@onready var animation_tree = $Path2D/AnimationTree
+const timeScale = "parameters/TimeScale/scale"
+
+@export var state = true
+@export var advance = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if state:
+		animation_tree.set(timeScale, 1)
+	else:
+		#animation_player.stop(true)
+		animation_tree.set(timeScale, 0)
+	animation_player.seek(advance)
+
+func switch():
+	state = !state
+	if state:
+		animation_tree.set(timeScale, 1)
+	else:
+		animation_tree.set(timeScale, 0)
